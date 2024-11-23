c938321aeeabd1440eda7fe4a89056b2f72d8cc7
Filippo Marcon <filippomarcon66@gmail.com>
2024-11-23 16:31:53 +0100

    edges HashSet inside model

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,8 +1,10 @@
+use std::collections::HashSet;
+
 use node_kind::NodeKind;
 use node_representation::NodeRepresentation;
 use ratatui::widgets::ListState;
 use screen::Screen;
-use wg_2024::config::Config;
+use wg_2024::{config::Config, network::NodeId};
 
 pub mod node_kind;
 pub mod node_representation;
@@ -12,25 +14,47 @@ pub mod screen;
 pub struct Model {
     pub screen: Screen,
     pub nodes: Vec<NodeRepresentation>,
+    pub edges: HashSet<(NodeId, NodeId)>,
     pub node_list_state: ListState,
 }
-impl Model{
+impl Model {
     pub fn new(cfg: &Config) -> Self {
         let mut nodes: Vec<NodeRepresentation> = Vec::new();
-    
-        for d in cfg.drone.iter() {
-            nodes.push(NodeRepresentation::new_from_cfgdrone(d));
-        }
-        for s in cfg.server.iter() {
-            nodes.push(NodeRepresentation::new_from_cfgserver(s));
-        }
-        for c in cfg.client.iter() {
-            nodes.push(NodeRepresentation::new_from_cfgclient(c));
-        }
-        Self {
+        let mut edges: HashSet<(NodeId, NodeId)> = HashSet::new();
+
+        let mut model =  Self {
             node_list_state: ListState::default(),
             screen: Screen::default(),
             nodes,
+            edges,
+        };
+
+        for d in cfg.drone.iter() {
+            model.nodes.push(NodeRepresentation::new_from_cfgdrone(d));
+            for to in d.connected_node_ids.iter(){
+                model.add_edge(d.id, *to);
+            }
+        }
+        for s in cfg.server.iter() {
+            model.nodes.push(NodeRepresentation::new_from_cfgserver(s));
+            for to in s.connected_drone_ids.iter(){
+                model.add_edge(s.id, *to);
+            }
+        }
+        for c in cfg.client.iter() {
+            model.nodes.push(NodeRepresentation::new_from_cfgclient(c));
+            for to in c.connected_drone_ids.iter(){
+                model.add_edge(c.id, *to);
+            }
+        }
+        model
+    }
+
+    pub fn add_edge(&mut self, from: NodeId, to: NodeId) {
+        if from < to {
+            self.edges.insert((from, to));
+        } else if to < from {
+            self.edges.insert((to, from));
         }
     }
 
@@ -53,7 +77,4 @@ impl Model{
             None
         }
     }
-
-    
-    
 }
